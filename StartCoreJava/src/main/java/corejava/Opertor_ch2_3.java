8a47757a49b9a7fc5bd54e2e93752ec3eaba7d1d
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-24 00:32:56 +0530

    oprater work with this vlaue

--- StartCoreJava/src/main/java/corejava/Opertor_ch2_3.java
+++ StartCoreJava/src/main/java/corejava/Opertor_ch2_3.java
@@ -4,6 +4,7 @@ public class Opertor_ch2_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//this operater gives +-*/with = to value*= this type
   int a=4;
   int b=9;
    b *= 3;

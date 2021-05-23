a8dd55eb4e0a6f23c79e5a294c047e8454fb5a03
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-23 22:48:06 +0530

    enter Student Details from himself

--- StartCoreJava/src/main/java/corejava/PeopleName_ch1_4.java
+++ StartCoreJava/src/main/java/corejava/PeopleName_ch1_4.java
@@ -1,9 +1,22 @@
 package corejava;
+import java.util.Scanner;
 
 public class PeopleName_ch1_4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your name:=");
+		String str =sc.toString();
+		System.out.println("Enter your father name:"+str);
+		String str1 =sc.toString();
+		System.out.println("enter your roll no:"+str1);
+		int a1=sc.nextInt();
+		System.out.println("roll no"+a1); 
+		
+		
+		
+		
 
 	}
 

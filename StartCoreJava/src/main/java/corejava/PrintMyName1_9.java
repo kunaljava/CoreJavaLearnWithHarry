6a112e201685ba83f9fe907a603a2509e9c6ab9f
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-23 23:50:16 +0530

    Taking user input

---
+++ StartCoreJava/src/main/java/corejava/PrintMyName1_9.java
@@ -0,0 +1,12 @@
+package corejava;
+import java.util.Scanner;
+
+public class PrintMyName1_9 {
+public static void main(String[] args) {
+	System.out.println("what is your name:");
+	Scanner sc=new Scanner(System.in);
+	String name=sc.next();
+	System.out.println("my name is " + name + " Welcome sir");
+	
+}
+}

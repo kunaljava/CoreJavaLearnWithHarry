4e177fdc873448f2234cfdc88a055b90ab197e40
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-23 23:30:25 +0530

    Add three no

---
+++ StartCoreJava/src/main/java/corejava/AddThreeNo1_7.java
@@ -0,0 +1,12 @@
+package corejava;
+
+public class AddThreeNo1_7 {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=30;
+		int sum=a+b+c;
+		System.out.println(sum);
+	}
+
+}

ade3ec91f02e3b05d90152661afa8a9b1b584a32
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-24 00:09:01 +0530

    Enter integer boolean value

---
+++ StartCoreJava/src/main/java/corejava/EnterTrueno1_10.java
@@ -0,0 +1,18 @@
+package corejava;
+import java.util.Scanner;
+
+public class EnterTrueno1_10 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		// TODO Auto-generated method stub
+		//hasnext is uset to user input which shows true or false
+    boolean b1 =sc.hasNextInt();
+    System.out.println(b1);
+    
+    //boolean b2 =sc.has();
+   // System.out.println(b2);
+   
+	}
+
+}

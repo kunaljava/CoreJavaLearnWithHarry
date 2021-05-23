51c65ae84394725a56a815de5091bcd6b28f5cb5
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-23 23:41:24 +0530

    Student test marks

---
+++ StartCoreJava/src/main/java/corejava/Cgpa_ch1_8.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class Cgpa_ch1_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float sum1=45;
+	float sum2=95;
+	float sum3=48;
+	 float Cgpa=(sum1+sum2+sum3)/30;
+ System.out.println(Cgpa);
+	}
+
+}

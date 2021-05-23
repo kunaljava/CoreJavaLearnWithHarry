e159d37e20ccddd08828583000f06ed1669e4390
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-23 22:32:15 +0530

    its give boolean value

---
+++ StartCoreJava/src/main/java/corejava/Bollean_ch1_3.java
@@ -0,0 +1,14 @@
+package corejava;
+import java.util.Scanner;
+
+public class Bollean_ch1_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		boolean b1=sc.hasNext();
+		System.out.println(b1);
+
+	}
+
+}

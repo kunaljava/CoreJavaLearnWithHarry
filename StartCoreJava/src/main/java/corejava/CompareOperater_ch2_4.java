a7fe2ea0fb273d02be803d10741dcc29c5995236
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-24 09:53:35 +0530

    compare two operater

---
+++ StartCoreJava/src/main/java/corejava/CompareOperater_ch2_4.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class CompareOperater_ch2_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a=45;
+    int b=25;
+    	//System.out.println(45>25 && 45>25);
+    System.out.println(45>25 || 45>95);
+	}
+
+}

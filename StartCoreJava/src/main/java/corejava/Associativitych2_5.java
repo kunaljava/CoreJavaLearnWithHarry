d5cc64542d5fff698ac2e14b4f48c890433e0c1f
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-24 10:53:07 +0530

    precendence rules are follows

---
+++ StartCoreJava/src/main/java/corejava/Associativitych2_5.java
@@ -0,0 +1,9 @@
+package corejava;
+
+public class Associativitych2_5 {
+public static void main(String[] args) {
+	//precedence rules also follows first high presedence value evaluate after that second 
+	int a=35/5-25*2;
+	System.out.println(a);
+}
+}

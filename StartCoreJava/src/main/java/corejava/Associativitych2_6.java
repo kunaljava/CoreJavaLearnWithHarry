13d20fd343b7bebad131c59ff9be299e2a94ce6e
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-24 15:25:35 +0530

    associativity  can perform other than that

---
+++ StartCoreJava/src/main/java/corejava/Associativitych2_6.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class Associativitych2_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=60;
+int y=12;
+int k=x*y/2;
+System.out.println(k);
+	}
+
+}

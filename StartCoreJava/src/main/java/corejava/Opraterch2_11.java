5da3d0d4a70ffd7bb9f68950ada92011ff2f3163
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-24 15:55:06 +0530

    Operation test can perform

---
+++ StartCoreJava/src/main/java/corejava/Opraterch2_11.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class Opraterch2_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //long a=25;
+		long a=2536545;
+		double b=2545635;
+		double c=a+b;
+		System.out.println(c);
+
+
+}}
--- StartCoreJava/src/main/java/corejava/ResultingDataTypech_2_10.java
+++ StartCoreJava/src/main/java/corejava/ResultingDataTypech_2_10.java
@@ -4,7 +4,7 @@ public class ResultingDataTypech_2_10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     byte a=10;
+     int a=10;
      int b=20;
      short c=35;
      int d=a+b+c;

3ac538f0b2c49fd74ba5e2378a36fdc3b08a530e
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-24 21:52:18 +0530

    practise set for java

--- StartCoreJava/src/main/java/corejava/PracticeSetch2_17.java
+++ StartCoreJava/src/main/java/corejava/PracticeSetch2_17.java
@@ -5,6 +5,7 @@ public class PracticeSetch2_17 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         char grade ='b';
+        //we can do data typeing by adding with give multiple them into that value
 
         grade=(char)(grade+8);
         System.out.println(grade);

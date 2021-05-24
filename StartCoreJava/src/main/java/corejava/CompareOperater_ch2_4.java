8c009c908a390726ed519e88e38b3b5505ee3e6f
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-24 10:52:17 +0530

    presendence rules are first evaulate first

--- StartCoreJava/src/main/java/corejava/CompareOperater_ch2_4.java
+++ StartCoreJava/src/main/java/corejava/CompareOperater_ch2_4.java
@@ -4,6 +4,8 @@ public class CompareOperater_ch2_4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// int 6%4=2 modulo operater
+		// int 4.8%1.1 return decimal opetrater
     int a=45;
     int b=25;
     	//System.out.println(45>25 && 45>25);

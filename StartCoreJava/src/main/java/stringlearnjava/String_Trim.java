4e72b3ebfbbd903525c76925710034a7c5f580df
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-25 13:40:53 +0530

    trim String

---
+++ StartCoreJava/src/main/java/stringlearnjava/String_Trim.java
@@ -0,0 +1,8 @@
+package stringlearnjava;
+
+public class String_Trim {
+public static void main(String[] args) {
+	String nontrimmedstring="      rajesh                 ";
+	System.out.println(nontrimmedstring.trim());
+}
+}

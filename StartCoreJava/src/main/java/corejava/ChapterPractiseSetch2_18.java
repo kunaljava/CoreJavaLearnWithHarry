fea38042768a3258694d5d935960035c74983e62
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-25 11:30:12 +0530

    for practise purpose

---
+++ StartCoreJava/src/main/java/corejava/ChapterPractiseSetch2_18.java
@@ -0,0 +1,11 @@
+package corejava;
+import java.util.Scanner;
+
+public class ChapterPractiseSetch2_18 {
+public static void main(String[] args) {
+	//Scanner sc =new Scanner(System.in);
+	//int a=sc.nextInt();
+	//System.out.println(a<5);
+	System.out.println(9*95/7+35/7);
+}
+}

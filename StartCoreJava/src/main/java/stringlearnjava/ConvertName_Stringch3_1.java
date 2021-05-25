ecc13908c19921cb5b341a2a63b6fdf5334b94ae
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-25 19:34:22 +0530

    to change that String

--- StartCoreJava/src/main/java/stringlearnjava/ConvertName_Stringch3_1.java
+++ StartCoreJava/src/main/java/stringlearnjava/ConvertName_Stringch3_1.java
@@ -3,8 +3,12 @@ package stringlearnjava;
 public class ConvertName_Stringch3_1 {
 	public static void main(String[] args) {
 		String name = "Rajesh Agrawal";
-		name=name.toLowerCase();
-		System.out.println(name);
+	//	name=name.toLowerCase();
+	//	System.out.println(name);
+		//problem 2 "to lower  case"
+		String text ="rajesh agrawal prem agrawal";
+	text=text.replaceAll(" ","_");
+	System.out.println(text);
 	}
 
 }

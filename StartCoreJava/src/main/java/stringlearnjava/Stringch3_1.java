408f5f742b92eaead9efc2bea118b9757e0ae649
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-25 10:14:50 +0530

    its first chapter of String

---
+++ StartCoreJava/src/main/java/stringlearnjava/Stringch3_1.java
@@ -0,0 +1,14 @@
+package stringlearnjava;
+import java.util.Scanner;
+
+public class Stringch3_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc=new Scanner(System.in);
+   // String name=new String("kunal");
+    String name="kunal singh";
+    System.out.println(name);
+	}
+
+}

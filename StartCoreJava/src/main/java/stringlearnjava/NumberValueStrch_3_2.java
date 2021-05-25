add646a84bb573986f550aa74bfecb3cf97468ca
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-25 13:48:13 +0530

    replace character with eath other

---
+++ StartCoreJava/src/main/java/stringlearnjava/NumberValueStrch_3_2.java
@@ -0,0 +1,10 @@
+package stringlearnjava;
+
+public class NumberValueStrch_3_2 {
+	public static void main(String[] args) {
+		String name ="kunal singh";
+		//System.out.println(name.substring(3,9));
+		System.out.println(name.replace('n', 's'));
+	}
+
+}

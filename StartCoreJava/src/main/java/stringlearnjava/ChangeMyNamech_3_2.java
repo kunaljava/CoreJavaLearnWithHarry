2e2319bcba6b0827f4ef6e4263f5421f5872bca6
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-25 20:32:55 +0530

    practise set for String

---
+++ StartCoreJava/src/main/java/stringlearnjava/ChangeMyNamech_3_2.java
@@ -0,0 +1,23 @@
+package stringlearnjava;
+
+public class ChangeMyNamech_3_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String name="to my Best                   Freind";
+     //  name=name.replaceAll("","_");
+      /* System.out.println(name);
+       String letter="dear <||>.,thanks a lot:";
+       letter=letter.replace("<||>", "kunal");
+       System.out.println(letter);*/
+       //problems 4
+       String myString="this String contain  double and triple  and triple spaces";
+       System.out.println(myString.indexOf("  "));
+       			//problem write a letter 
+       String myletter="dear kunal,\n\t your effort is sufficeint for learning core java course enougn try more\n\t thanks";
+       System.out.println(myletter);
+       String emplyee ="dear sir,\n \ti am unable join my classes today in running of fever please teach me next time \n \tthanks a lot";
+         System.out.println(emplyee);
+
+	}
+}
--- StartCoreJava/src/main/java/stringlearnjava/ConvertName_Stringch3_1.java
+++ StartCoreJava/src/main/java/stringlearnjava/ConvertName_Stringch3_1.java
@@ -5,7 +5,8 @@ public class ConvertName_Stringch3_1 {
 		String name = "Rajesh Agrawal";
 	//	name=name.toLowerCase();
 	//	System.out.println(name);
-		//problem 2 "to lower  case"
+		//problem 2 "to lower  case"changed Stgring we have  variable update that String
+		// we have not 
 		String text ="rajesh agrawal prem agrawal";
 	text=text.replaceAll(" ","_");
 	System.out.println(text);

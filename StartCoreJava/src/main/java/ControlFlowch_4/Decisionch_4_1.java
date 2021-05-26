90ae359a22b60cf0db58d812f57be1bba3156cf2
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-26 11:45:30 +0530

    decition making programs

---
+++ StartCoreJava/src/main/java/ControlFlowch_4/Decisionch_4_1.java
@@ -0,0 +1,15 @@
+package ControlFlowch_4;
+
+public class Decisionch_4_1 {
+	public static void main(String[] args) {
+		int age=6;
+		if(age>18) {
+			System.out.println("yoy are eligible to give vote :=");
+			
+		}
+		else {
+			System.out.println("you are not eligible to give vote");
+		}
+	}
+
+}

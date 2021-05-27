3c145e63b16cdb87072b9a34a5f2d79fef69e227
kunal <kunal@DESKTOP-7PP74DJ>
2021-05-27 11:34:46 +0530

    recurtion chapter revision set

--- StartCoreJava/src/main/java/oopspractiseSet_5/Recurtionch5_3.java
+++ StartCoreJava/src/main/java/oopspractiseSet_5/Recurtionch5_3.java
@@ -1,5 +1,20 @@
-package oopspractiseSet_5;
+ package oopspractiseSet_5;
 
 public class Recurtionch5_3 {
-
+	static int factorial(int n) {
+		//factorial(n)=n*n-1*n......1
+		//Facatorial(n)5*4*3*2*1
+		if(n==0 || n==1) {
+			return 1;
+		}
+		else {
+			return n*factorial( n = n-1);
+		}
+	}
+	public static void main(String[] args) {
+	 
+		int n=9;
+		System.out.println("the value factorial is :"+factorial(n)); 
+		
+	}
 }
